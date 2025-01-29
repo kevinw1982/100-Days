182a1e2d76eb9c755aeb9bf053ffc0d357a148fd
Kevin Wainwright <84409709+kevinw1982@users.noreply.github.com>
2025-01-29 18:40:19 +0000

    Day one complete

---
+++ 1/main.py
@@ -0,0 +1,6 @@
+#This is the day one Project file.
+
+print("Welcome to the band name generator")
+city = input("Which City did you grow up in?\n")
+pet = input("What's your pets name?\n")
+print(f"Your band name could be {city} {pet}")
---
+++ 1/task.py
@@ -0,0 +1,13 @@
+#these are the tasks for day one covering inputs and variables!
+
+print("Hello World!\nHello Kevin!\nHello Everyone!")
+print("Hello "+"Kevin!")
+
+name = input("What is your name?")
+print(f"Hi {name} i am your Acer")
+
+print("The length of the word typed in is: " + str(len(name)))
+
+username = input("What is your name?")
+length = len(username)
+print(length)
